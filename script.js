09f4779ae34e1989f4f36075d91a221e96717021
jagrati gupta <123jagratigupta@gmail.com>
2022-05-12 20:04:34 +0530

    file-uploded-add

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+var body = document.querySelector("body");
 var btn = document.getElementById("submit_btn");
 var chats =document.getElementById("chats");
-console.log(date)
+
 var chat_text=
        `<div class="massage incoming">
             <p class="mb-0">hii  </p>
@@ -37,7 +37,7 @@ var chat_text=
         </p>
         <p class="time p-0 ps-5">11:30</p>
        
-    </div>`;
+        </div>`;
 var date = new Date();
 var hours = date.getHours();
 var min = date.getMinutes();
@@ -56,10 +56,9 @@ hours = (hours % 12) || 12; //get 12 format hours..
     else{
         min=min;
     }
-console.log(hours,min);    
-
-
+   
     // Get new outgoing msg..
+    //msg send while user click on Send btn...
     btn.addEventListener("click",function(){
 
     if(document.getElementById("inpt").value ==""){
@@ -76,4 +75,36 @@ console.log(hours,min);
     chats.innerHTML=chat_text;
     document.getElementById("inpt").value="";
 
-});
+    });
+    
+    //msg send while user click on Enter key...
+    var input = document.getElementById("inpt");
+        input.addEventListener("keyup", function(event) {
+            console.log(event);
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("submit_btn").click();
+            }
+    });
+
+    //While user click on Input-file button..
+    document.getElementById("file-input").addEventListener("change", function() {
+        
+        if (document.getElementById("file-input").value) {
+               alert("File uploaded")
+            } else {
+              alert("No file chosen, yet");
+            }
+    });
+
+    // function ScrollDiv(){
+
+        // if(document.getElementById('chats').scrollTop<(document.getElementById('chats').scrollHeight-document.getElementById('chats').offsetHeight)){-1
+        //       document.getElementById('chats').scrollTop=document.getElementById('chats').scrollTop+1
+        //       }
+        // else {
+        //     document.getElementById('chats').scrollTop=0;
+        //}
+        //}
+     
+    //  setInterval(ScrollDiv,50);
